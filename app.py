44b903f61a34ceeeae31bf5113442e8328018eac
rogeriotanabe <rogerio.tanabe@yahoo.com.br>
2025-03-17 14:47:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,12 @@ import dash
 from dash import html
 
 app = dash.Dash(__name__)
-
 server = app.server  # Expose the server variable
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
-    html.Div(children='''Dash: A web application framework for Python.''')
+    html.Div(children='''Dash: A new web application framework for Python.''')
 ])
 
 if __name__ == "__main__":
-    #app.run_server(debug=True, host='0.0.0.0', port=8050)
     app.run_server(debug=False, host='0.0.0.0', port=8050)
